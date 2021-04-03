3aba4c17171a3cadfdeb01ab0abe5611e14ef0ab
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-03 20:31:03 +0900

    自動再生実装

--- app/assets/javascripts/post_video.js
+++ app/assets/javascripts/post_video.js
@@ -1,49 +1,60 @@
 $(document).ready(function() {
-  const postContent = document.querySelector('.post-content');
-  const video = document.querySelector('.postVideo');
+  const postVideos = document.querySelectorAll('li .postVideo');
+  // const postVideo = document.getElementById('postVideo');
+  let video = postVideos[0];
   const mute = document.getElementById('mute');
   const height = $('.post-content').css("height");
   console.log(height)
-  // console.log(postContent);
-  
+  console.log(postVideos);
+  // console.log(postContents);
+  console.log(video);
 
-  mute.addEventListener('click', function(e) {
-    video.muted = !video.muted;
-  });
+  video.play();
 
-  // video.autoplay = true;
-
-  // video.addEventListener('ended', () => {
-  //   alert('video ended!!!!');
-  // })
-
-  
-
-
-  const juice = document.querySelector('.orange-juice');
-  const btn = document.querySelector('#playPause');
-
-  function togglePlayPause() {
-    if(video.paused) {
-      btn.className = "pause";
+  // 最初の動画の再生が終わったら次の動画を再生する
+  let index = 0;
+  video.addEventListener('ended', function() {
+    index++;  
+    if(index < postVideos.length) {
+      let video = postVideos[index];
       video.play();
-    }
-    else {
-      btn.className = "play";
-      video.pause();
-    }
-  }
-
-  btn.onclick = function() {
-    togglePlayPause();
-  };
-  
-  video.addEventListener('timeupdate', function() {
-    const juicePos = video.currentTime / video.duration;
-    juice.style.width = juicePos * 100 + "%";
-    if(video.end) {
-      btn.className = "play";
-    }
+    } 
   });
+  
+
+  // mute.addEventListener('click', function(e) {
+  //   video.muted = !video.muted;
+  // });
+
+
+
+  
+
+
+  // const juice = document.querySelector('.orange-juice');
+  // const btn = document.querySelector('#playPause');
+
+  // function togglePlayPause() {
+  //   if(video.paused) {
+  //     btn.className = "pause";
+  //     video.play();
+  //   }
+  //   else {
+  //     btn.className = "play";
+  //     video.pause();
+  //   }
+  // }
+
+  // btn.onclick = function() {
+  //   togglePlayPause();
+  // };
+  
+  // video.addEventListener('timeupdate', function() {
+  //   const juicePos = video.currentTime / video.duration;
+  //   juice.style.width = juicePos * 100 + "%";
+  //   if(video.end) {
+  //     btn.className = "play";
+  //   }
+  // });
 });
 
