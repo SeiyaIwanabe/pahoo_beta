02d71494cb38ea7b2df4d4bff122b1ae6d80d249
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-04 15:22:24 +0900

    jsファイル整理

--- app/assets/javascripts/post_video.js
+++ app/assets/javascripts/post_video.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  const postVideos = document.querySelectorAll('li .postVideo');
+  // const postVideos = document.querySelectorAll('li .postVideo');
   // const postVideo = document.getElementById('postVideo');
-  let video = postVideos[0];
+  const video = document.querySelector('.postVideo');
+  // const contentHeight = $('.post-content').css("height");
+  // console.log(contentHeight);
+  console.log(video);
+  const btn = document.querySelector('#playPause');
   const mute = document.getElementById('mute');
-  const contentHeight = $('.post-content').css("height");
-  console.log(contentHeight);
-  // console.log(postContents);
   const juice = document.querySelector('.orange-juice');
   
-  // 最初の動画を自動再生
-  video.play();
   
   // ProgressBarを表示
   video.addEventListener('timeupdate', function() {
@@ -20,43 +19,45 @@ $(document).ready(function() {
     }
   });
 
-  // 最初の動画の再生が終わったら次の動画を再生する
-  let index = 0;
-  video.addEventListener('ended', function() {
-    scrollTo(0, contentHeight);
-    index++;  
-    if(index < postVideos.length) {
-      let video = postVideos[index];
-      video.play();
-    } 
-  });
   
-  const btn = document.querySelector('#playPause');
-
+  
   // mute.addEventListener('click', function(e) {
-  //   video.muted = !video.muted;
-  // });
-
-
-
-  
-
+  //     video.muted = !video.muted;
+  //   });
 
 
   // function togglePlayPause() {
-  //   if(video.paused) {
-  //     btn.className = "pause";
+    //   if(video.paused) {
+      //     btn.className = "pause";
+      //     video.play();
+      //   }
+      //   else {
+        //     btn.className = "play";
+        //     video.pause();
+        //   }
+        // }
+        
+        // btn.onclick = function() {
+          //   togglePlayPause();
+          // };
+    
+    
+    
+    
+    
+    
+    
+  // 最初の動画を自動再生
+  // video.play();
+
+  // 最初の動画の再生が終わったら次の動画を再生する
+  // let index = 0;
+  // video.addEventListener('ended', function() {
+  //   scrollTo(0, contentHeight);
+  //   index++;  
+  //   if(index < postVideos.length) {
+  //     let video = postVideos[index];
   //     video.play();
-  //   }
-  //   else {
-  //     btn.className = "play";
-  //     video.pause();
-  //   }
-  // }
-
-  // btn.onclick = function() {
-  //   togglePlayPause();
-  // };
-  
+  //   } 
+  // });
 });
-
