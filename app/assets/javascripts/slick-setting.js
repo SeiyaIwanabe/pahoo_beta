82c6a7c215a607fbaa457dae06956436d5a70e42
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-05 09:05:32 +0900

    バナーUI修正

--- app/assets/javascripts/slick-setting.js
+++ app/assets/javascripts/slick-setting.js
@@ -1,22 +1,20 @@
-$(document).ready(function() {
-  $('.bannerArea').slick({
-    infinite: true,
-    slidesToShow: 3,
-    // slidesToScroll: 3,
-    centerMode: true,
-    centerPadding: '50px',
-    swipe: true,
-    // カクつきを軽減
-    // cssEase: 'linear'
-    // arrows: false
-    swipeToSlide: true
-  });
+// $(document).ready(function() {
+//   $('.bannerArea').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     // slidesToScroll: 3,
+//     centerMode: true,
+//     centerPadding: '50px',
+//     swipe: true,
+//     // arrows: false
+//     swipeToSlide: true
+//   });
 
-  // センターのバナーが大きくなる関数
-  $('.bannerArea').on('beforeChange', function(){
-    $('.slick-center').removeClass('is--active');
-  });
-  $('.bannerArea').on('afterChange', function(){
-    $('.slick-center').addClass('is--active');
-  });
-});
+//   // センターのバナーが大きくなる関数
+//   $('.bannerArea').on('beforeChange', function(){
+//     $('.slick-center').removeClass('is--active');
+//   });
+//   $('.bannerArea').on('afterChange', function(){
+//     $('.slick-center').addClass('is--active');
+//   });
+// });
