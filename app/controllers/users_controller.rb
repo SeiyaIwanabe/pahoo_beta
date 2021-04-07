639cdba12fedc78f6bab2a13e0aad65a9a8db1e0
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-07 18:44:32 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # @genres = current_user.tags.ids
-    # @genres = Tag.find(params[:tag_id]).users
   end
 
   def edit
