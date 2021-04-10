23b1f31613f71206a73b4e00391d683defcbbb60
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-10 09:36:31 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Welcome to Pahoo!!"
+      redirect_to root_path, notice: "#{@user.nickname}さん、ようこそ！"
     else
       render :new
     end
