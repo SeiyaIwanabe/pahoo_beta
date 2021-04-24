97ff0638415e3df2b8f39555dc43d27388514e83
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-24 15:55:25 +0900

    DM画面デザイン修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,7 +3,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    @entry2 = Entry.create(entry_params)
     redirect_to "/rooms/#{@room.id}"
   end
 
@@ -12,10 +12,17 @@ class RoomsController < ApplicationController
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @messages = @room.messages
       @message = Message.new
-      @entries = @room.entries
+      @entries = @room.entries.where.not(user_id: current_user.id)
+      # メッセージを送る相手のレコードを取得
+      # @target = Entry.find_by(user_id: @room.user.id, room_id: @room.id)
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 
 
