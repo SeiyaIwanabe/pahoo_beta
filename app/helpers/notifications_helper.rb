3943150c2dfc94a2f6f0192c94118c7cc2ce27ac
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-04-24 19:50:46 +0900

    通知機能実装

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,17 +2,18 @@ module NotificationsHelper
 
   def notification_form(notification)
     @comment = nil
-    visitor = link_to notification.visitor.nickname, notification.visitor, style:"font-weight: bold;"
+    visitor = link_to notification.visitor.nickname, notification.visitor, class: "visitorName"
     your_post = link_to 'あなたの投稿', notification.post, style:"font-weight: bold;"
     case notification.action
       when "follow" then
-        "#{visitor}さんがあなたをフォローしました"
+        "#{visitor}さんがあなたにマッチング申請をしました！プロフィールを覗いてみましょう"
       when "favorite" then
         "#{visitor}さんが#{your_post}にいいね！しました"
       when "comment" then
         @comment = Comment.find_by(id: notification.comment_id)&.content
         "#{visitor}さんが#{your_post}にコメントしました"
       when "DM" then
+        @message = Message.find_by(id: notification.message_id)&.text
         "#{visitor}さんがあなたにメッセージを贈りました!"
     end
   end
