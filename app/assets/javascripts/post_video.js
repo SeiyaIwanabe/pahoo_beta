1abaa2d113f24e0521c233e2199de0c64651a457
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-05-06 16:47:50 +0900

    home画面にプログレスバー設置

--- app/assets/javascripts/post_video.js
+++ app/assets/javascripts/post_video.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   const video = document.querySelectorAll('.postVideo');
   const $btn = document.querySelectorAll('#playPause');
-  // const mute = document.getElementById('mute');
   const juice = document.querySelector('.orange-juice');
   
   
@@ -30,10 +29,12 @@ $(document).ready(function() {
     const $target_label = $this.closest('.btnLabel');
     const $target_video = $target_label.parentNode.firstElementChild;
     const $target_mute_btn = $target_label.nextElementSibling;
+    const $target_video_bar = $target_video.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild;
     console.log('$this', $this);
     console.log('$target_label', $target_label);
     console.log('$target_video', $target_video);
     console.log('$target_mute_btn', $target_mute_btn);
+    console.log('$target_video_bar', $target_video_bar);
 
 
     //動画の再生/停止
@@ -56,15 +57,14 @@ $(document).ready(function() {
       togglePlayPause();
     };
 
-
     // ProgressBarを表示
-    // $target_video.addEventListener('timeupdate', function() {
-    //   const juicePos = $target_video.currentTime / $target_video.duration;
-    //   juice.style.width = juicePos * 100 + "%";
-    //   if($target_video.end) {
-    //     $this.className = "play";
-    //   }
-    // });
+    $target_video.addEventListener('timeupdate', function() {
+      const $juicePos = $target_video.currentTime / $target_video.duration;
+      $target_video_bar.style.width = $juicePos * 100 + "%";
+      if($target_video.end) {
+        $this.className = "play";
+      }
+    });
   }
 
   // 全$btnに関数を適用
