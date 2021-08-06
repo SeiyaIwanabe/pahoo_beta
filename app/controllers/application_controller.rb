1c6ab5ed24313faabb758e11d8d2ff33fe4e8949
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-08-06 23:06:04 +0900

    未ログイン時リダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   #新規登録時のストロングパラメータに「nicknameカラム」の追加
   def configure_permitted_parameters
