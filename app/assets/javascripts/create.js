3df51255821dc5bb3853487abdc4ee87b6efe20e
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2021-08-15 08:50:55 +0900

    修正

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 
     // リンクをクリック時にナビゲーション出現
     $create_link.addEventListener('click', () => {
-      $('#createNav').animate({ "margin-bottom": 0 }, 200);
+      $('#createNav').animate({ "margin-bottom": 0 }, 500);
       $('#js-nav-close').css('display', 'block');
     })
 
     // 周りの黒背景をクリックしたらメニューと黒背景が閉じる
     $('#js-nav-close').click(function() {
-      $('#createNav').animate({ "margin-bottom": -500 }, 200);
+      $('#createNav').animate({ "margin-bottom": -500 }, 500);
       $(this).css('display', 'none');
     });
 
